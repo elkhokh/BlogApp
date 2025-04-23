e76dcd71d61fd1ef8cfdda2cc4fbfa02b5aa2f1e
mostapha khalid <mustafaelkhokhy@gamil.com>
2025-04-23 17:10:27 +0200

    new project using database

---
+++ config/db.php
@@ -0,0 +1,35 @@
+<?php
+// try and catch php with database
+
+try{
+$conn=mysqli_connect("localhost",'root',"","blog_app");
+if (!$conn){
+ 
+    header("location: ./view/maintenance.php");
+    exit;
+}
+
+}
+catch(Exception $ex){
+        
+    header("location: ./view/maintenance.php");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// if(!$conn) {
+//     echo "not select database";
+// }
+// if(!$conn)
+// {
+// die("failed connection database".mysqli_connect_error());
+// }
